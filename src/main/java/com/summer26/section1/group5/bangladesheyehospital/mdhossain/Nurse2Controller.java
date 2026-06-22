3767088e1e4c5744104f37a13ce6e261db3334e1
Mdhossain567 <mdhossainmubc420@gmail.com>
2026-06-22 16:02:13 +0600

    Add nurse dashboard FXML

---
+++ src/main/java/com/summer26/section1/group5/bangladesheyehospital/mdhossain/Nurse2Controller.java
@@ -0,0 +1,18 @@
+package com.summer26.section1.group5.bangladesheyehospital.mdhossain;
+
+import javafx.event.ActionEvent;
+
+public class Nurse2Controller
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void showDataOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void backbuttononclick(ActionEvent actionEvent) {
+    }
+}
