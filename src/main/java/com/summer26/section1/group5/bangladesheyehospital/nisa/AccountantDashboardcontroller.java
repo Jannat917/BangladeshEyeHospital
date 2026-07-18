64b43a9469dac33475ba8fc2a122633ba7a46051
nisa25306 <jannatulnisa25@gmail.com>
2026-07-18 21:31:27 +0600

    Accountant Dashboard created

---
+++ src/main/java/com/summer26/section1/group5/bangladesheyehospital/nisa/AccountantDashboardcontroller.java
@@ -0,0 +1,42 @@
+package com.summer26.section1.group5.bangladesheyehospital.nisa;
+
+import javafx.event.ActionEvent;
+
+public class AccountantDashboardcontroller
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void financialreportOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void receivePaymentOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void manageexpenseOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void refundOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void salaryOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void paysupplierOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void generatebillOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void duepaymentOA(ActionEvent actionEvent) {
+    }
+}
