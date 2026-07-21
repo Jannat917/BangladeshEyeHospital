4d121c7c3dc47321a6cfbf979ee41767eb24b4bf
Md Hossain <mdhossainmubc420@gmail.com>
2026-07-21 23:59:23 +0600

    Add securitystaff dashboard FXML

---
+++ src/main/java/com/summer26/section1/group5/bangladesheyehospital/mdhossain/securitystaffDashboardController.java
@@ -0,0 +1,7 @@
+package com.summer26.section1.group5.bangladesheyehospital.mdhossain;
+
+public class securitystaffDashboardController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
