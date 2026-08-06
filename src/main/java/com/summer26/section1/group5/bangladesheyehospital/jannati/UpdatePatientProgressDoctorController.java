6b248ecaad586a76218cf719079dbade281bac8e
Jannati <jannat51923@gmail.com>
2026-08-06 14:31:54 +0600

    Consultaion histroy updated

---
+++ src/main/java/com/summer26/section1/group5/bangladesheyehospital/jannati/UpdatePatientProgressDoctorController.java
@@ -0,0 +1,39 @@
+package com.summer26.section1.group5.bangladesheyehospital.jannati;
+
+public class UpdatePatientDoctorController
+{
+    @javafx.fxml.FXML
+    private TextField patientIdTextField;
+    @javafx.fxml.FXML
+    private TextField doctorTextField;
+    @javafx.fxml.FXML
+    private TextArea conditionTextArea;
+    @javafx.fxml.FXML
+    private TextArea medicationTextArea;
+    @javafx.fxml.FXML
+    private TextArea remarksTextArea;
+    @javafx.fxml.FXML
+    private TextField patientNameTextField;
+    @javafx.fxml.FXML
+    private Label messageLabel;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void searchButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void clearButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void backButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void saveUpdateButton(ActionEvent actionEvent) {
+    }
+}
